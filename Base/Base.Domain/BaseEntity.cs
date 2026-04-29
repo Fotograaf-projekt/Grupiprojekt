52630c65402b68e58b73de4bde87171feb19163d
reesi <reesim@taltech.ee>
2026-04-29 10:42:35 +0300

    proov

--- Base/Base.Domain/BaseEntity.cs
+++ Base/Base.Domain/BaseEntity.cs
@@ -7,4 +7,5 @@ public abstract class BaseEntity : BaseEntity<int>
 public abstract class BaseEntity<TKey> where TKey : IEquatable<TKey>
 {
     public TKey Id { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
 }
