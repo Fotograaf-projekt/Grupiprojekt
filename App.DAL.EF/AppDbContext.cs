a0d7be9d507e26709d65fce4eccefef3060103ab
lisannagontmacher-creator <lisann@taltech.ee>
2026-05-13 09:35:41 +0300

    Availability, ContactMessage, Client klassid ja AppDbContext uuendamine

--- App.DAL.EF/AppDbContext.cs
+++ App.DAL.EF/AppDbContext.cs
@@ -11,6 +11,9 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     public DbSet<SocialLink> SocialLinks => Set<SocialLink>();
     public DbSet<Service> Services => Set<Service>();
     public DbSet<Booking> Bookings => Set<Booking>();
+    public DbSet<Client> Clients => Set<Client>();
+    public DbSet<ContactMessage> ContactMessages => Set<ContactMessage>();
+    public DbSet<Availability> Availabilities => Set<Availability>();
    
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -33,6 +36,27 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
             .HasForeignKey(s => s.PhotographerId)
             .OnDelete(DeleteBehavior.Cascade);
 
-     
+        builder.Entity<ContactMessage>()
+            .HasOne(m => m.Client)
+            .WithMany(c => c.ContactMessages)
+            .HasForeignKey(m => m.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<ContactMessage>()
+            .HasOne(m => m.Service)
+            .WithMany()
+            .HasForeignKey(m => m.ServiceId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Client>()
+            .HasMany(c => c.Bookings)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Availability>()
+            .HasOne(a => a.Booking)
+            .WithOne()
+            .HasForeignKey<Availability>(a => a.BookingId)
+            .OnDelete(DeleteBehavior.SetNull);
     }
 }
