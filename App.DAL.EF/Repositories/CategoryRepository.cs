c133e48dd60de807f43d98751d680fcfcd02f404
martajohanna <martas@taltech.ee>
2026-05-18 21:39:55 +0300

    marta - albumid ja kategooriad asendunud portfoolioga

--- App.DAL.EF/Repositories/CategoryRepository.cs
+++ App.DAL.EF/Repositories/CategoryRepository.cs
@@ -1,12 +1,18 @@
 using App.Domain;
 using Base.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.DAL.EF.Repositories;
 
 public interface ICategoryRepository : IRepository<Category>
 {
+    Task<List<Category>> AllWithAlbumsAsync();
 }
 
 public class CategoryRepository(AppDbContext db) : EFBaseRepository<Category>(db), ICategoryRepository
 {
+    public Task<List<Category>> AllWithAlbumsAsync() =>
+        Set.Include(c => c.Albums!)
+           .ThenInclude(a => a.Photos)
+           .ToListAsync();
 }
